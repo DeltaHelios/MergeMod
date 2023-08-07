3ec7e1fbfd85506aa4acdf0f08b380dcfb1fad5b
DeltaHelios <stefan.v.iancu@gmail.com>
2023-08-07 16:11:13 +0300

    Initial commit

---
+++ src/main/java/net/deltahelios/mergemod/MergeModClient.java
@@ -0,0 +1,10 @@
+package net.deltahelios.mergemod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MergeModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient(){
+
+}
+}
