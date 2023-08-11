2221d10bc2437ab67f0b25c927997de668594c5f
DeltaHelios <stefan.v.iancu@gmail.com>
2023-08-11 08:20:20 +0300

    Fixing TropicInvoker

--- src/main/java/net/deltahelios/mergemod/item/costum/TropicInvokerItem.java
+++ src/main/java/net/deltahelios/mergemod/item/costum/TropicInvokerItem.java
@@ -40,14 +40,18 @@ public class TropicInvokerItem extends Item {
             BlockPos positionClicked = context.getBlockPos();
             PlayerEntity player = context.getPlayer();
 
-                RegistryKey<ConfiguredFeature<?, ?>> registryKey = TreeConfiguredFeatures.MEGA_JUNGLE_TREE;
 
-                RegistryEntry registryEntry = context.getWorld().getRegistryManager().get(RegistryKeys.CONFIGURED_FEATURE).getEntry(registryKey).orElse(null);
-                ConfiguredFeature configuredFeature = (ConfiguredFeature)registryEntry.value();
-                BlockState blockState = context.getWorld().getFluidState(positionClicked).getBlockState();
-                context.getWorld().setBlockState(positionClicked, blockState, Block.NO_REDRAW);
-                        context.getWorld().updateListeners(positionClicked, context.getWorld().getBlockState(positionClicked), blockState, Block.NOTIFY_LISTENERS);
+            RegistryKey<ConfiguredFeature<?, ?>> registryKey = TreeConfiguredFeatures.MEGA_JUNGLE_TREE;
 
+            RegistryEntry registryEntry = context.getWorld().getRegistryManager().get(RegistryKeys.CONFIGURED_FEATURE).getEntry(registryKey).orElse(null);
+            ConfiguredFeature configuredFeature = (ConfiguredFeature)registryEntry.value();
+            BlockState blockState = context.getWorld().getFluidState(positionClicked).getBlockState();
+            context.getWorld().setBlockState(positionClicked, blockState, Block.NO_REDRAW);
+            if (configuredFeature.generate(player.getServer().getWorld(context.getWorld().getRegistryKey()), player.getServer().getWorld(context.getWorld().getRegistryKey()).getChunkManager().getChunkGenerator(), Random.create(0), positionClicked)) {
+                if (context.getWorld().getBlockState(positionClicked) == blockState) {
+                    context.getWorld().updateListeners(positionClicked, context.getWorld().getBlockState(positionClicked), blockState, Block.NOTIFY_LISTENERS);
+                }
+            }
 
         }
         context.getStack().damage(1, context.getPlayer(), playerEntity -> playerEntity.sendToolBreakStatus(playerEntity.getActiveHand() ));
